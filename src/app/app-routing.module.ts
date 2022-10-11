a190750e5a3cdc9e653b2f6c0a42e579e333fcc3
Nadeera Nimesha <Nadeeranimesha456@gmail.com>
2022-10-11 23:19:41 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { HomeComponent } from './Components/home/home.component';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './Components/add/add.component';
 import { ViewComponent } from './Components/view/view.component';
@@ -7,9 +7,9 @@ import { ViewComponent } from './Components/view/view.component';
 const routes: Routes = [
 
   { path: '',redirectTo:'Home', pathMatch:'full' },
-  { path: '/Home', component:HomeComponent},
-  { path: '/Add', component:AddComponent},
-  { path: '/View', component:ViewComponent},
+  { path: 'Home', component : HomeComponent},
+  { path: 'Add', component : AddComponent},
+  { path: 'View', component : ViewComponent},
 
 ];
 
