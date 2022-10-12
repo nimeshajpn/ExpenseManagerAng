e1c8d87cdbe7d7805bcc51d689696c313784f3e5
Nadeera Nimesha <Nadeeranimesha456@gmail.com>
2022-10-12 23:20:51 +0530

    .

--- src/app/service/expense-service.service.ts
+++ src/app/service/expense-service.service.ts
@@ -1,10 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ModelExpense } from '../model/model-expense.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseServiceService {
- Data?:ModelExpense;
-  constructor() { }
+ 
+   private url:string='http:localhost:9000/';
+
+  constructor(httpclient:HttpClient) {
+
+
+
+   }
+
+   public getAll():Observable<IContact[]>
+   {
+
+        let data:string = '${this.url}/expense';
+
+      return 
+   }
 }
